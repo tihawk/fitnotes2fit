d206896f2cb833cfc77c2a92278ff324aca72d15
Kiril Blagoev <kblagoev@vub.de>
2022-04-22 14:37:17 +0300

    added support for heart rate and strava relative effort

--- src/main/java/com/developination/fitnotes2fit/App.java
+++ src/main/java/com/developination/fitnotes2fit/App.java
@@ -1,12 +1,18 @@
 package com.developination.fitnotes2fit;
 
 import com.developination.fitnotes2fit.ActivityEncoder.ActivityEncoder;
+import com.developination.fitnotes2fit.util.NoiseGenerator;
 
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        NoiseGenerator noiseGenerator = new NoiseGenerator();
         ActivityEncoder.CreateExampleStrengthActivity();
+        // for (int i = 0; i <= 60; i++) {
+        //     System.out.println(noiseGenerator.noise(i));
+        //     System.out.println(noiseGenerator.noise(i, 90, 110));
+        // }
     }
 }
