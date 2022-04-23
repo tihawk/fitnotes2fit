0c6f80fd0fe52088181e535e20eb2ab88406f4ec
Kiril Blagoev <kblagoev@vub.de>
2022-04-23 20:25:24 +0300

    Created interactive options for average heart-rate and average rest time. by default they are no longer generated. Closes #2

--- src/main/java/com/developination/fitnotes2fit/controllers/Convert.java
+++ src/main/java/com/developination/fitnotes2fit/controllers/Convert.java
@@ -16,15 +16,53 @@ public class Convert implements Runnable {
   private String file;
   @Option(names = {"--output", "-o"}, defaultValue = "", description = "Output folder for the generated .fit files.")
   private String outFolder;
+  @Option(
+    names = {"--heartrate", "-hr"},
+    arity = "0..1", 
+    interactive = true, 
+    description = "Average heart-rate for your workouts. Useful, for example, to calculate Relative Effort metric in Strava.")
+  private short avgHeartRate;
+  @Option(
+    names = {"--resttime", "-rt"},
+    arity = "0..1",
+    interactive = true,
+    description = "Average rest time between sets in minutes (e.g. 1.5). Don't think too hard on it, in the output it's randomised within plus-minus 1 minute. Same as for the heart-rate, this is useful when calculating Relative Effort and other metrics."
+  )
+  private float avgRestTime;
 
   @Override
   public void run() {
+
+    if (this.avgHeartRate == 0) {
+      String s = System.console().readLine("Would you like to enter average heart-rate for your workouts? Useful, for example, to calculate Relative Effort metric in Strava. (default 0): ");
+      try {
+        if (!s.isEmpty()) {
+          this.avgHeartRate = Short.valueOf(s);
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    System.out.printf("Average Heart-rate=%s%n", this.avgHeartRate);
+
+    if (this.avgRestTime == 0) {
+      String s = System.console().readLine("Would you like to enter average rest time between sets? Don't think too hard on it, in the output it's randomised within plus-minus 1 minute. Same as for the heart-rate, this is useful when calculating Relative Effort and other metrics. Enter in minutes (e.g. 1.5) (default 0): ");
+      try {
+        if (!s.isEmpty()) {
+          this.avgRestTime = Float.valueOf(s);
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    System.out.printf("Average Rest time=%s%n", this.avgRestTime);
+
     System.out.println( "On it!" );
     try {
         List<Activity> activityList = FitNotesParser.parseFileNotesIntoActivities(file);
         for (Activity activity : activityList) {
             System.out.println("[main] Starting to encode activity: " + activity.getActivityName());
-            ActivityEncoder encoder = new ActivityEncoder(activity);
+            ActivityEncoder encoder = new ActivityEncoder(activity, avgHeartRate, avgRestTime);
             encoder.encodeActivity(outFolder);
         }
     } catch (Exception e) {
